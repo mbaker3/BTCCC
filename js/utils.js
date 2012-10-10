0ca68ef308b3307221b7672567a7e3b20a868bc4
mbaker_karman <mbaker@karmaninteractive.com>
2012-10-09 23:15:00 -0400

    BTCC Meeting 2 and 3
    - Moved project out of cloud 9 and into CSS Deck (one file now)
    - Base entity and exists
    - LocomotiveEntity Created (Paul, the colourful ones)
    - Color Eater Entity Created (Mike, the black blobs)
    - world and critter layer are rendering
    - startings of a control panel now come in on hover
    - world can now be a rectangle
    - fixed alpha blending on ColorEater

--- js/utils.js
+++
@@ -1,9 +0,0 @@
-var UTILS = {
-    setPixel: function (imageData, x, y, r, g, b, a) {
-        index = (x + y * imageData.width) * 4;
-        imageData.data[index+0] = r;
-        imageData.data[index+1] = g;
-        imageData.data[index+2] = b;
-        imageData.data[index+3] = a;
-    }
-};
